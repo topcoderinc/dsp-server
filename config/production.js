90b85317840751ebaad30258676851675f3d7faf
Kyle Bowerman <kyle.bowerman@gmail.com>
2016-11-08 13:42:26 -0600

    copied default to production config

--- config/production.js
+++ config/production.js
@@ -4,16 +4,20 @@
 'use strict';
 
 /**
- * The production configuration file.
+ * The default configuration file.
  *
- * @author      TCSCODER
- * @version     1.0
+ * @author      kbowerma
+ * @version     1.1
  */
 
 module.exports = {
-  PORT: process.env.PORT || 3000,
+  LOG_LEVEL: process.env.LOG_LEVEL || 'debug',
+  PORT: process.env.PORT || 3500,
+  AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || 'nope',
+  SALT_WORK_FACTOR: 2,
+  TOKEN_EXPIRES: 10 * 60 * 60,
   db: {
-    url: process.env.MONGOLAB_URI,
-    poolSize: 10,
+    url: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/drones',
+    poolSize: 5,
   },
 };
