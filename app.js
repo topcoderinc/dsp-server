797285d222ecd9316a544200b184a4406e648e62
Kyle Bowerman <kyle.bowerman@gmail.com>
2016-11-22 14:10:01 -0600

    added swagger-ui-express,   use this command to generate the json file ```swagger-codegen generate -i swagger.yaml -l swagger```

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 app.set('port', config.PORT);
 
 app.use(bodyParser.json());
@@ -64,6 +67,7 @@ _.each(require('./routes'), (verbs, url) => {
 
 app.use('/', apiRouter);
 app.use(errorMiddleware());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 http.listen(app.get('port'), () => {
   winston.info(`Express server listening on port ${app.get('port')}`);
