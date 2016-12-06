40e4bf01f6882240da580581ad2bb828019e7578
nardevorg <vedran@nardev.org>
2016-12-07 00:02:32 +0100

    Fix login and missions endpoints

--- controllers/MissionController.js
+++ controllers/MissionController.js
@@ -15,11 +15,48 @@ const MissionService = require('../services/MissionService');
 
 // Exports
 module.exports = {
+  create,
+  update,
   search,
   getSingle,
   monthlyCountByDrone,
   getAllByDrone,
+  download,
 };
+
+/**
+ * Create mission in the system
+ *
+ * @param req the request
+ * @param res the response
+ */
+function* create(req, res) {
+  res.json(yield MissionService.create(req.body));
+}
+
+/**
+ * Update mission in the system
+ *
+ * @param req the request
+ * @param res the response
+ */
+function* update(req, res) {
+  res.json(yield MissionService.update(req.params.id, req.body));
+}
+
+/**
+ * Download mission data
+ *
+ * @param req the request
+ * @param res the response
+ */
+function* download(req, res) {
+  const data = yield MissionService.download(req.params.id);
+  const json = JSON.stringify(data.missionFile);
+  res.setHeader('Content-disposition', `attachment; filename=${data.name}.mission`);
+  res.setHeader('Content-type', 'application/json');
+  res.send(json);
+}
 
 /**
  * Search missions in the system
