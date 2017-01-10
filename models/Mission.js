98086a30fb7f3e08463928d3c4bfa049e7b62d59
gondzo <klox>
2017-01-10 16:01:05 +0100

    set mission answers index to sparse (unique if not null)

--- models/Mission.js
+++ models/Mission.js
@@ -44,7 +44,7 @@ const ZoneSchema = new Schema({
 
 // Pilot checklist answer schema
 const PilotChecklistAnswerSchema = new Schema({
-  question: {type: Schema.Types.ObjectId, required: true, unique: true, ref: 'Question'},
+  question: {type: Schema.Types.ObjectId, required: true, unique: true, sparse: true, ref: 'Question'},
   answer: {type: String, enum: _.values(enums.PilotChecklistAnswers)},
   note: String,
 });
